19d79bfa9970531b0872b0656f223f015e33bc77
mserino <m.serino@hotmail.it>
2015-02-02 16:36:40 +0000

    the form checks if the user has replied yes or no to the first question

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+
 	$('#submit').on('click', function(){
-		$('#formValue').text($('input[name=select]:checked', '#needItSelection').val());
+		// $('#formValue').text($('input[name=select]:checked', '#needItSelection').val());
+		var needItValue = $('input[name=select]:checked', '#needItSelection').val();
+		if ( needItValue === 'yes') {
+			$('#formValue').text('APPROVED');
+		} else {
+			$('#formValue').text('DENIED');
+		}
 	});
 });
